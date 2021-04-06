ffc69128b661c7767f4396b605b9a40f0e6680ed
GalaxyTheoryz <Patrick.fox1307@outlook.com>
2021-04-06 11:29:01 +0100

    Create dmuserplugin.py

---
+++ dmuserplugin/dmuserplugin.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel
+
+class dmuser(commands.Cog): 
+    """DMs a user a message"""
+    
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def dm(ctx, user_id=None, *, args=None):
+     if user_id != None and args != None:
+        try:
+            target = await bot.fetch_user(user_id)
+            await target.send(args)
+
+            await ctx.channel.send("'" + args + "' sent to: " + target.name)
+
+        except:
+            await ctx.channel.send("Couldn't dm the given user.")
+
+def setup(bot):
+    bot.add_cog(dmuser(bot))
