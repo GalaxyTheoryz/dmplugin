5c6ac75e4092f4e9149b653351c5767ae490de8f
GalaxyTheoryz <Patrick.fox1307@outlook.com>
2021-04-06 10:53:59 +0100

    Create DMUserPlugin.py

---
+++ dmusersplugin/DMUserPlugin.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel
+
+class DMUser(commands.Cog): 
+    """DMs a user a message"""
+    
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def dm(ctx, user_id=None, *, args=None):
+     if user_id != None and args != None:
+        try:
+            target = await bot.fetch_user(user_id)
+            await target.send(args)
+
+            await ctx.channel.send("'" + args + "' sent to: " + target.name)
+
+        except:
+            await ctx.channel.send("Couldn't dm the given user.")
